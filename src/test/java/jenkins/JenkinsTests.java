a13fc6060b4959a8575e40fadcb39e16ff32454d
Burhan Dundar <burhandundar@gmail.com>
2022-03-20 14:08:31 +0300

    Initial commit

---
+++ src/test/java/jenkins/JenkinsTests.java
@@ -0,0 +1,53 @@
+package jenkins;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class JenkinsTests {
+
+
+
+    @Test
+    public void apiTest(){
+
+        RequestSpecification spec  = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").build();
+
+        //Set the base url and path params
+        spec.pathParams("first", "booking", "second",3);
+
+
+        //Send the Get request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+
+
+        response.prettyPrint();
+
+
+        Map<String, Object> actualData = response.as(HashMap.class);
+        System.out.println(actualData);
+
+        Map<String, Object> bookingdates = (Map)actualData.get("bookingdates");
+
+
+        assertEquals("Jim", actualData.get("firstname"));
+        assertEquals("Jackson", actualData.get("lastname"));
+        assertEquals(964, actualData.get("totalprice"));
+        assertEquals(false, actualData.get("depositpaid"));
+
+        assertEquals("2015-05-13", bookingdates.get("checkin"));
+        assertEquals("2019-10-27", bookingdates.get("checkout"));
+
+    }
+
+
+
+}
